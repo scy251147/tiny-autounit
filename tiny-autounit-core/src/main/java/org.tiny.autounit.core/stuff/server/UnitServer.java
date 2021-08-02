92d05ecb042cfa95b6ea0610be063ef35bf92cd1
shichaoyang <shichaoyang@jd.com>
2021-08-02 15:52:43 +0800

    add comment

--- tiny-autounit-core/src/main/java/org.tiny.autounit.core/stuff/server/UnitServer.java
+++ tiny-autounit-core/src/main/java/org.tiny.autounit.core/stuff/server/UnitServer.java
@@ -3,6 +3,7 @@ package org.tiny.autounit.core.stuff.server;
 /**
  * @author shichaoyang
  * @Description: mock服务端，用于放置mock数据，供客户端获取
+ * @Note 此server可以用http服务来做，需要额外的使用tomcat来启动，为了保持core的即开即用特性，以netty server端为主
  * @date 2021-08-02 15:49
  */
 public class UnitServer {
