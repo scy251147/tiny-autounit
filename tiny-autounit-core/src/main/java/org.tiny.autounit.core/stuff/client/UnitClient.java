6635039220b10c3e37c5a09feaabcb8a3c4df7fc
shichaoyang <shichaoyang@jd.com>
2021-08-02 15:49:56 +0800

    add unitclient & unitserver

---
+++ tiny-autounit-core/src/main/java/org.tiny.autounit.core/stuff/client/UnitClient.java
@@ -0,0 +1,9 @@
+package org.tiny.autounit.core.stuff.client;
+
+/**
+ * @author shichaoyang
+ * @Description: mock客户端，用于从服务端拿取mock数据
+ * @date 2021-08-02 15:48
+ */
+public class UnitClient {
+}
---
+++ tiny-autounit-core/src/main/java/org.tiny.autounit.core/stuff/server/UnitServer.java
@@ -0,0 +1,9 @@
+package org.tiny.autounit.core.stuff.server;
+
+/**
+ * @author shichaoyang
+ * @Description: mock服务端，用于放置mock数据，供客户端获取
+ * @date 2021-08-02 15:49
+ */
+public class UnitServer {
+}
