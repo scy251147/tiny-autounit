3fe02dd86d7da11a18289466c3dc16728a350935
shichaoyang <shichaoyang@jd.com>
2021-08-02 16:20:33 +0800

    add comments

--- tiny-autounit-core/src/main/java/org.tiny.autounit.core/strategy/UnitRequestHandleStrategy.java
+++ tiny-autounit-core/src/main/java/org.tiny.autounit.core/strategy/UnitRequestHandleStrategy.java
@@ -4,7 +4,7 @@ import org.tiny.autounit.core.model.UnitClassMethod;
 
 /**
  * @author shichaoyang
- * @Description: 处理入参部分
+ * @Description: 处理入参部分，此部分内容来源需要请求mock server获取
  * @date 2021-07-30 17:31
  */
 public class UnitRequestHandleStrategy implements IUnitBuildStrategy {
